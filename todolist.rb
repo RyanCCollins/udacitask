3a87ca77a5efd8ba3885421f916a7f9a04cd4821
Ryan Collins <rccproductions@mac.com>
2016-02-28 14:05:52 -0500

    Feat: add date to string, string to date conversion

--- todolist.rb
+++ todolist.rb
@@ -11,19 +11,41 @@ class TodoList
     @file = @title + ".json"
   end
 
-  def add_new_item(new_item, *due_date)
+  def add_new_item(new_item, due_date)
     item = Item.new(new_item, due_date)
     @items << item
   end
 
   def load_from_file
-    file = File.read(@file)
-    item_array = JSON.parse(file)
-    item_array.each do |item|
-      date = Date.parse(item["due_date"])
-      item = Item.new(item["description"], date, item["completion_status"])
-      @items << item
+    # Protects against the file being empty
+    if File.zero?(@file)
+      return
     end
+    file = File.read(@file)
+    item_array = get_parsed_json
+    puts item_array
+    unless item_array == nil
+      item_array.each do |item|
+        date = item["due_date"].as_date
+        item = Item.new(item["description"], date, item["completion_status"])
+        @items << item
+      end
+    end
+  end
+
+  def get_parsed_json
+    file = File.read(@file)
+    return JSON.parse(file)
+  rescue JSON::ParserError => e
+    return nil
+  end
+
+  def items_to_json
+    array = Array.new
+    @items.each do |item|
+      array << item.hash_me
+    end
+    return array.to_json
   end
 
   def output_to_file!(output)
@@ -37,14 +59,6 @@ class TodoList
     @items.delete_at(index)
   end
 
-  def items_to_json
-    array = Array.new
-    @items.each do |item|
-      array << item.hash_me
-    end
-    return array.to_json
-  end
-
   def change_status(index, status)
     @items[index].update_status!(status)
   end
@@ -56,7 +70,7 @@ class TodoList
     @items.each_with_index do |item, index|
       output << "#{index + 1} - #{item.description}\n"
       output << "Status: #{item.is_complete? ? "Complete" : "Not Complete"}\n"
-      output << "Due Date: #{item.due_date}\n"
+      output << "Due Date: #{item.due_date.as_string}}\n"
       output << "Overdue: #{item.is_overdue? ? "Yes" : "No"}\n\n"
     end
     output_to_file!(output)
@@ -68,11 +82,13 @@ class Item
   attr_reader :description, :due_date
   attr_accessor :completion_status
 
-  def initialize(item_description, due_date = Time.now.to_date, completion_status = "Not Complete")
+  # Initialize with variables.  Due_date MUST be a string.
+    # Normall
+  def initialize(item_description, due_date, completion_status = false)
     @description = item_description
     @completion_status = completion_status
-    @due_date = due_date
-    puts due_date
+    date = DateTime.new
+    @due_date = due_date.as_date
   end
 
   def update_status!(complete)
@@ -89,7 +105,23 @@ class Item
   end
 
   def is_overdue?
-    now = Time.now.to_date()
+    now = DateTime.now
     return @due_date > now
+  end
+end
+
+#This is probably not a good idea.
+  # It is not actually mutating, but just handling the translation
+  # From dates to string.  I need to learn more about type safety in Ruby and
+  # Would love to hear some strategies you use for error handling.
+class DateTime
+  def as_string
+    return DateTime.parse(self.to_s).strftime("%m/%d/%Y")
+  end
+end
+
+class String
+  def as_date
+    return DateTime.strptime(self, "%m/%d/%Y")
   end
 end
--- udacitask.rb
+++ udacitask.rb
@@ -3,11 +3,11 @@ require_relative 'todolist.rb'
 # Creates a new todo list
 list = TodoList.new("Todo")
 # Add four new items
-list.add_new_item("Submit the rest of the Ruby projects", "2014-02-28")
-list.add_new_item("Submit project 2-3 of FE", "2016-03-16")
-list.add_new_item("Submit Capstone", "2016-03-03")
-list.add_new_item("Do other awesome projects")
-list.load_from_file()
+list.add_new_item("Submit the rest of the Ruby projects", "02/28/2015")
+list.add_new_item("Submit project 2-3 of FE", "02/28/2016")
+list.add_new_item("Submit Capstone", "03/03/2016")
+list.add_new_item("Do other awesome projects", "03/28/2016")
+list.load_from_file
 # Print the list
 list.output!
 # Delete the first item
